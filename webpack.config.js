97e9443040996725c5710fdade3cd781e4916335
Abby Christens <abby.christens@cbc.ca>
2019-10-23 14:54:07 -0400

    Adding sass support

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,42 @@
 const webpack = require("webpack");
 const path = require("path");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     mode: "production",
     entry: "./src/index.js",
     output: {
-        filename: "custom-button.js",
-        library: "custom-button",
-        libraryTarget: "umd",
-        path: path.resolve(__dirname, "dist")
+        filename: 'custom-button.js',
+        library: 'custom-button',
+        libraryTarget: 'umd',
+        path: path.resolve(__dirname, 'dist')
     },
     module: {
         rules: [
             {
-                test /\.js$/,
+                test: /\.js$/,
                 exclude: /node_modules/,
-                user: {
-                    loader: "babel_loader",
+                use: {
+                    loader: "babel-loader",
                     options: {
                         presets: ["@babel/preset-env", "@babel/preset-react"]
                     }
                 }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'sass-loader'
+                    }
+                ]
             }
         ]
     },
@@ -30,6 +44,7 @@ module.exports = {
         minimizer: [new UglifyJsPlugin()]
     },
     plugins: [
-        new CleanWebpackPlugin(["dist"])
+        new CleanWebpackPlugin()
+
     ]
 }
